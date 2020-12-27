216543c89bbbf488c1c88d2e913413bb23511124
Muhammad Hamza Asif <ihijazi313@gmail.com>
2020-12-27 09:13:30 -0800

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Layout from "../components/Layout"
+
+export default function Custom404() {
+    return (
+        <Layout title="404 Page Not Found" >
+            <h1>404 - Page Not Found</h1>
+        </Layout>
+    )
+  }
